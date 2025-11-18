865714ed2f30e277c66f55a653db13e689432489
Carlos Robles <carlos.robles.dominguez.al@iespoligonosur.org>
2025-11-18 09:16:11 +0100

    .

--- AppExamenCarlos/urls.py
+++ AppExamenCarlos/urls.py
@@ -1,8 +1,3 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [
-    path('', views.base, name='base'),
-    # URL videojuegos por genero y pais
-    path('videojuegos/<str:genero>/<str:pais_origen>/', views.videojuegos_por_genero_pais, name='videojuegos_por_genero_pais.html'),
-]
