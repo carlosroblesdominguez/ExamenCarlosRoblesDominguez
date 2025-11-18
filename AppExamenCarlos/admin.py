406fa8d8f9b81839df11d546f7bf9ee7b4b0b39d
Carlos Robles <carlos.robles.dominguez.al@iespoligonosur.org>
2025-11-18 08:36:15 +0100

    .

--- AppExamenCarlos/admin.py
+++ AppExamenCarlos/admin.py
@@ -2,8 +2,3 @@ from django.contrib import admin
 from .models import *
 
 # Register your models here.
-admin.site.register(Plataforma)
-admin.site.register(Analisis)
-admin.site.register(Sede)
-admin.site.register(Estudio)
-admin.site.register(Videojuego)
